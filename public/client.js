ad4afa28e58a0bcfc1041aa30ac2f17c95b8fbb8
Julian Lam <julian@nodebb.org>
2021-12-06 12:48:47 -0500

    fix: manually re-do the mobile menu, fixes #8

--- public/client.js
+++ public/client.js
@@ -1,19 +1,19 @@
 'use strict';
 
-/*
-	Hey there!
-
-	This is the client file for your theme. If you need to do any client-side work in javascript,
-	this is where it needs to go.
-
-	You can listen for page changes by writing something like this:
-
-	  $(window).on('action:ajaxify.end', function(ev, data) {
-		var url = data.url;
-		console.log('I am now at: ' + url);
-	  });
-*/
-
-$(document).ready(function () {
-	// Your code goes here
+require(['hooks'], (hooks) => {
+	hooks.on('action:app.load', () => {
+		try {
+			const mobileToggleEl = document.querySelector('.elementor-element-940fadf .elementor-menu-toggle');
+			const dropdownEl = mobileToggleEl.parentNode.querySelector('.elementor-nav-menu--dropdown');
+			const dropdownRect = dropdownEl.getBoundingClientRect();
+			mobileToggleEl.addEventListener('click', function () {
+				this.classList.toggle('elementor-active');
+				dropdownEl.style.left = `-${dropdownRect.x}px`;
+				dropdownEl.style.top = `${dropdownRect.y + 10}px`;
+				dropdownEl.style.width = '100vw';
+			})
+		} catch (e) {
+			console.log('[theme/community] Unable to find mobile menu toggle/dropdown');
+		}
+	})
 });
